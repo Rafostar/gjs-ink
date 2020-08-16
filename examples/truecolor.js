8e904541ffa65fb7d69008b07d4386c0232dddef
Rafostar <40623528+Rafostar@users.noreply.github.com>
2020-08-16 09:31:12 +0200

    Fix iteration below 0 in truecolor example

--- examples/truecolor.js
+++ examples/truecolor.js
@@ -4,13 +4,14 @@ let printer = new Ink.Printer();
 
 for(let i = 0; i <= 2; i++) {
     let str = '';
+    let step = 4;
     let array = [0, 0, 0];
     array[i] = 255;
 
-    while(array[i] >= 0) {
+    while(array[i] >= step) {
         printer.background = Ink.colorFromRGB(array);
         str += printer.getPainted(' ');
-        array[i] -= 4;
+        array[i] -= step;
     }
     print(str);
 }
